d9f8563a7372f419c44f71045794e06720bcbb70
immora <kasia_pi1@wp.pl>
2020-04-26 21:29:55 +0200

    added theme picker and region selector

--- myOApp/myOApp/Views/AboutPage.xaml.cs
+++ myOApp/myOApp/Views/AboutPage.xaml.cs
@@ -1,18 +1,44 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using myOApp.Controls;
+using myOApp.Themes;
+using myOApp.ViewModels;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace myOApp.Views
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class AboutPage : ContentPage
     {
         public AboutPage()
         {
             InitializeComponent();
+            BindingContext = new AboutViewModel();
+        }
+
+        void OnThemeChanged(System.Object sender, System.EventArgs e)
+        {
+            Picker picker = sender as Picker;
+            ThemeEnum theme = (ThemeEnum)picker.SelectedItem;
+
+            ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+            if (mergedDictionaries != null)
+            {
+                mergedDictionaries.Clear();
+
+                switch (theme)
+                {
+                    case ThemeEnum.Alternative:
+                        mergedDictionaries.Add(new AlternativeTheme());
+                        break;
+                    case ThemeEnum.Default:
+                    default:
+                        mergedDictionaries.Add(new DefaultTheme());
+                        break;
+                }
+                Debug.WriteLine($"Changed theme to: {theme.ToString()}");
+            }
         }
     }
 }
