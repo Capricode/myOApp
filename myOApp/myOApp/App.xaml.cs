880f84a840f5a631c207a99a0c54c582314b6c26
immora <kasia_pi1@wp.pl>
2020-05-05 22:04:18 +0200

    added Roboto Condensed and FontAwesome fonts

--- myOApp/myOApp/App.xaml.cs
+++ myOApp/myOApp/App.xaml.cs
@@ -1,9 +1,19 @@
-﻿using System;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using myOApp.Services;
 using myOApp.Views;
 
+[assembly: ExportFont("fa-regular-400.ttf", Alias = "FA-R")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FA-S")]
+[assembly: ExportFont("fa-brands-400.ttf", Alias = "FA-B")]
+
+[assembly: ExportFont("RobotoCondensed-Bold.ttf", Alias = "Roboto-B")]
+[assembly: ExportFont("RobotoCondensed-BoldItalic.ttf", Alias = "Roboto-BI")]
+[assembly: ExportFont("RobotoCondensed-Regular.ttf", Alias = "Roboto")]
+[assembly: ExportFont("RobotoCondensed-Italic.ttf", Alias = "Roboto-I")]
+[assembly: ExportFont("RobotoCondensed-Light.ttf", Alias = "Roboto-L")]
+[assembly: ExportFont("RobotoCondensed-LightItalic.ttf", Alias = "Roboto-LI")]
 namespace myOApp
 {
     public partial class App : Application
