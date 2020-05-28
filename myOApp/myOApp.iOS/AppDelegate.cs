1d3ef0e674ab3e747f2b5aa99d4bb7171ac87227
immora <kasia_pi1@wp.pl>
2020-05-28 21:30:51 +0200

    last fixes, forgotten icons(?)

--- myOApp/myOApp.iOS/AppDelegate.cs
+++ myOApp/myOApp.iOS/AppDelegate.cs
@@ -20,8 +20,6 @@ namespace myOApp.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
-
             global::Xamarin.Forms.Forms.Init();
 
             CachedImageRenderer.Init();
