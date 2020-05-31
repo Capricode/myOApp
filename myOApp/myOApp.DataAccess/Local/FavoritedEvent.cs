336dc60244a40de42dde45058fd672c1ae9b44a5
immora <kasia_pi1@wp.pl>
2020-05-31 13:49:48 +0200

    basedatabase and favorited event slqlite future

---
+++ myOApp/myOApp.DataAccess/Local/FavoritedEvent.cs
@@ -0,0 +1,14 @@
+﻿using myOApp.DataAccess.Database;
+using SQLiteNetExtensions.Attributes;
+
+namespace myOApp.DataAccess.Local
+{
+    public class FavoritedEvent
+    {
+        [ForeignKey(typeof(EventEntity))]
+        public string EventId { get; set; }
+
+        [OneToOne(CascadeOperations = CascadeOperation.CascadeRead)]
+        public EventEntity Event { get; set; }
+    }
+}
