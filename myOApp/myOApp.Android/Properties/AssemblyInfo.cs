01c1a0d9955ecd71401e9f23051f877c98b16fe8
immora <kasia_pi1@wp.pl>
2020-05-31 21:04:16 +0200

    changes for android publish to google play store

--- myOApp/myOApp.Android/Properties/AssemblyInfo.cs
+++ myOApp/myOApp.Android/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("myOApp.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -28,3 +28,10 @@ using Android.App;
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+
+// disable debugging https://docs.microsoft.com/en-us/xamarin/android/deploy-test/release-prep/#protect_app
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
