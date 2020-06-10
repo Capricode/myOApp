c0b61359a8ad6ab2f883c24ec35b45cf08315f69
immora <kasia_pi1@wp.pl>
2020-06-10 20:21:21 +0200

    fixed url to Capricode's website

--- myOApp/myOApp/ViewModels/AboutViewModel.cs
+++ myOApp/myOApp/ViewModels/AboutViewModel.cs
@@ -1,4 +1,5 @@
-﻿using myOApp.Services;
+﻿using myOApp.Definitions;
+using myOApp.Services;
 using System;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -30,10 +31,20 @@ namespace myOApp.ViewModels
             {
                 await this.DialogService.ShowMessage("We could not find e-mail app. Please try contacting us manually: contact@capricode.ch", "Alert");
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 await this.DialogService.ShowMessage("We could not open e-mail app. Please try contacting us manually: contact@capricode.ch", "Alert");
             }
+        }
+
+        ICommand goToWebsiteCommand;
+        public ICommand GoToWebsiteCommand => goToWebsiteCommand ?? (goToWebsiteCommand = new Command(async () => await ExecuteGoToWebsiteCommand()));
+
+        private async Task ExecuteGoToWebsiteCommand()
+        {
+            var websiteUrl = Constants.WebsiteUrl;
+            var uri = new Uri(websiteUrl);
+            await Browser.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
         }
     }
 }
