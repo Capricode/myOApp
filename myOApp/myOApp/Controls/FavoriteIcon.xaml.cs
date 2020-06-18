29aff436e3d53aabd2900cad09acae10061960ce
immora <kasia_pi1@wp.pl>
2020-06-18 15:18:48 +0200

    added separate control for Favorite Icon

---
+++ myOApp/myOApp/Controls/FavoriteIcon.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace myOApp.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FavoriteIcon : Label
+    {
+        public FavoriteIcon()
+        {
+            InitializeComponent();
+        }
+    }
+}
